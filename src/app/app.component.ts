9eecd3ede23ca7ef296eeb1d0831921582833283
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-19 12:44:07 -0400

    add new comps, update styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { RecListComponent } from "./rec-list/rec-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent, RecListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/rec-list/rec-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rec-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './rec-list.component.html',
+  styleUrl: './rec-list.component.scss'
+})
+export class RecListComponent {
+
+}

01633fe8bef300968117a445d0787f1b9724da6a
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-22 20:30:39 -0400

    add empty list view

--- src/app/rec-list/rec-list.component.ts
+++ src/app/rec-list/rec-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-rec-list',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './rec-list.component.html',
   styleUrl: './rec-list.component.scss'
 })

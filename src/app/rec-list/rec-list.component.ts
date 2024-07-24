ab13754721156f8601d4a8a7a73e6d74428c2d05
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-23 23:06:40 -0400

    scaffold subregion list comp

--- src/app/rec-list/rec-list.component.ts
+++ src/app/rec-list/rec-list.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RecsService } from '../recs.service';
 import { FilterStore } from '../filters.store';
+import { SubregionListComponent } from "./subregion-list/subregion-list.component";
 
 @Component({
   selector: 'app-rec-list',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, SubregionListComponent],
   templateUrl: './rec-list.component.html',
   styleUrl: './rec-list.component.scss'
 })
--- src/app/rec-list/subregion-list/subregion-list.component.ts
+++ src/app/rec-list/subregion-list/subregion-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-subregion-list',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './subregion-list.component.html',
   styleUrl: './subregion-list.component.scss'
 })
 export class SubregionListComponent {
-  
+
 }

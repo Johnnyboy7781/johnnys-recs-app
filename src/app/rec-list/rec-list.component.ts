cef9e78840a6ab6ebfb642ff04bba63559731df3
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-27 18:00:26 -0400

    add animation for list data being populated

--- src/app/rec-list/rec-list.component.ts
+++ src/app/rec-list/rec-list.component.ts
@@ -6,13 +6,31 @@ import { ListStore } from '../list.store';
 import { Place } from '../data-types';
 import { RecsService } from '../recs.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-rec-list',
   standalone: true,
   imports: [MatIconModule, SubregionListComponent, MatProgressSpinnerModule],
   templateUrl: './rec-list.component.html',
-  styleUrl: './rec-list.component.scss'
+  styleUrl: './rec-list.component.scss',
+  animations: [
+    trigger('fade', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate(
+              '300ms ease-out',
+              style({ opacity: 1 })
+            )
+          ], 
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class RecListComponent {
 
